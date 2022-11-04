994569e64bbf809dc5ee5d0781337398284dfb25
PriscilaButzke <priscilabutzke@outlook.com>
2022-11-04 18:43:32 -0300

    :construction: formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
+
 
 function App() {
+
+  const[colaboradores,setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores,colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Desenvolvimento"/>
+      <Time nome="QA Testes e qualidade de software"/>
+      <Time nome="Designer"/>
+      <Time nome="Pessoas e cultura"/>
+      
     </div>
   );
 }
--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 /* props é o nome dado geralmente a uma propriedade, mas pode ser qualquer nome  */
 const CampoTexto = (props) => {
@@ -9,11 +10,25 @@ const placeholdermodificada = `${props.placeholder}Texto a ser exibido junto`
 
 depois so chamar  <input placeholder={props.placeholderModificada}/>*/
 
+/*onChange é disparado quando os valores são inseridos na entrada
+O target é uma referência ao objeto que enviou o evento. Por exemplo, 
+quando você quer capturar o que foi digitado em um campo input de um form , você utiliza o event. */
+
+/*useState prepara uma pseudo variavel, gerando o objeto valor, para lermos e setValor para escrever e setar valores 
+quando passado pelo setValor ele passa por diversas verificações
+do ciclo de vida do objeto*/
+
+
+const aoDigitado = (evento)=>{
+   props.aoAlterado(evento.target.value)
+}
+
+
 
 return(
     <div className="campo-texto">
         <label>{props.label}</label>
-       <input required={props.obrigatorio} placeholder={props.placeholder}/>
+       <input value={props.valor} onChange= {aoDigitado}required={props.obrigatorio} placeholder={props.placeholder}/>
     </div>
 )
 }
